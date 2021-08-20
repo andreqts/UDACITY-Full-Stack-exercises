99569c35275cd7b127205c501dcee10e53fd1788
Andre Quites Ordovas Santos <andrequites@gmail.com>
2021-08-20 19:00:25 -0300

    added restrictions and __repr__ to data classes closes #6

--- projects/01_fyyur/worked_code/models.py
+++ projects/01_fyyur/worked_code/models.py
@@ -25,42 +25,51 @@ class Venue(db.Model):
     __tablename__ = 'Venue'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    genres = db.Column(db.String(120))
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    address = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
+    name = db.Column(db.String, nullable = False)
+    genres = db.Column(db.String(120), nullable = False)
+    city = db.Column(db.String(120), nullable = False)
+    state = db.Column(db.String(120), nullable = False)
+    address = db.Column(db.String(120), nullable = False)
+    phone = db.Column(db.String(120), nullable = False)
     looking_talent = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String(120))
     website_link = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    image_link = db.Column(db.String(500), nullable = False)
     facebook_link = db.Column(db.String(120))
     past_shows = db.relationship('Show', backref='venue', lazy=True)
     upcoming_shows = db.relationship('Show', backref='venue', lazy=True)
+
+    def __repr__(self):
+        return f'<Venue {self.id}: {self.name} phone: {self.phone}>'
 
 class Artist(db.Model):
     __tablename__ = 'Artist'
 
     id = db.Column(db.Integer, primary_key=True)
-    name = db.Column(db.String)
-    city = db.Column(db.String(120))
-    state = db.Column(db.String(120))
-    phone = db.Column(db.String(120))
+    name = db.Column(db.String, nullable = False)
+    city = db.Column(db.String(120), nullable = False)
+    state = db.Column(db.String(120), nullable = False)
+    phone = db.Column(db.String(120), nullable = False)
     looking_venue = db.Column(db.Boolean, default=False)
     seeking_description = db.Column(db.String(120))
-    genres = db.Column(db.String(120))
-    image_link = db.Column(db.String(500))
+    genres = db.Column(db.String(120), nullable = False)
+    image_link = db.Column(db.String(500), nullable = False)
     website_link = db.Column(db.String(120))
     facebook_link = db.Column(db.String(120))
     past_shows = db.relationship('Show', backref='artist', lazy=True)
     upcoming_shows = db.relationship('Show', backref='artist', lazy=True)
+
+    def __repr__(self):
+        return f'<Venue {self.id}: {self.name} phone: {self.phone}>'
 
 # TODO Implement Show and Artist models, and complete all model relationships and properties, as a database migration.
 class Show(db.Model):
     __tablename__ = 'Show'
 
     id = db.Column(db.Integer, primary_key=True)
-    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'))
-    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'))
-    start_datetime = db.Column(db.DateTime)
+    artist_id = db.Column(db.Integer, db.ForeignKey('Artist.id'), nullable = False)
+    venue_id = db.Column(db.Integer, db.ForeignKey('Venue.id'), nullable = False)
+    start_datetime = db.Column(db.DateTime, nullable = False)
+
+    def __repr__(self):
+        return f'<Venue {self.id}: from {self.artist_id} in {self.venue_id} at {self.start_datetime}>'
