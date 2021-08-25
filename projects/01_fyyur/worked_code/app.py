699141cc60c5b9d0925cd52fe7af1b29ce1aae44
Andre Quites Ordovas Santos <andrequites@gmail.com>
2021-08-25 19:10:08 -0300

    added search artist page #8

--- projects/01_fyyur/worked_code/app.py
+++ projects/01_fyyur/worked_code/app.py
@@ -155,18 +155,23 @@ def artists():
 
 @app.route('/artists/search', methods=['POST'])
 def search_artists():
-  # TODO: implement search on artists with partial string search. Ensure it is case-insensitive.
-  # seach for "A" should return "Guns N Petals", "Matt Quevado", and "The Wild Sax Band".
-  # search for "band" should return "The Wild Sax Band".
+
+  term_to_search = request.form.get('search_term', '')
+  artists_found = Artist.query.filter(Artist.name.ilike(f'%{term_to_search}%')).all()
+
+  artists_data = []
+  for artist in artists_found:
+    artists_data.append({
+      "id": artist.id,
+      "name": artist.name,
+      "num_upcoming_shows": artist.get_upcoming_shows_count(),
+    })
+
   response={
-    "count": 1,
-    "data": [{
-      "id": 4,
-      "name": "Guns N Petals",
-      "num_upcoming_shows": 0,
-    }]
+    "count": len(artists_found),
+    "data": artists_data
   }
-  return render_template('pages/search_artists.html', results=response, search_term=request.form.get('search_term', ''))
+  return render_template('pages/search_artists.html', results=response, search_term=term_to_search)
 
 @app.route('/artists/<int:artist_id>')
 def show_artist(artist_id):
