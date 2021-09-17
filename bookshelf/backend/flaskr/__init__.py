951e20a43c611e4ca369183f251d6e948d8d1027
André Quites Ordovás Santos <andrequites@gmail.com>
2021-09-17 15:47:02 -0300

    bookshelf - fixed http protocol on frontend and added custom error handling examples to backend

--- bookshelf/backend/flaskr/__init__.py
+++ bookshelf/backend/flaskr/__init__.py
@@ -57,7 +57,10 @@ def create_app(test_config=None):
         current_books = paginate_books(request, selection)
 
         if len(current_books) == 0:
-          abort(404)
+          page = request.args.get('page', 1, type=int)
+          msg = f'Page {page} not found in the database'
+          abort(404, msg)
+
 
         return jsonify({
           'success': True,
@@ -66,7 +69,13 @@ def create_app(test_config=None):
         })
     #return 'TODO: here I will return the books...'
 
-  
+  @app.errorhandler(404)
+  def not_found(error):
+    return jsonify({
+      'success': False,
+      'error': 404,
+      "message": f"Page does not exist! Error '{error}'"
+    }), 404
 
   # @TODO: Write a route that will update a single book's rating. 
   #         It should only be able to update the rating, not the entire representation
@@ -107,7 +116,33 @@ def create_app(test_config=None):
   # @TODO: Write a route that create a new book. 
   #        Response body keys: 'success', 'created'(id of created book), 'books' and 'total_books'
   # TEST: When completed, you will be able to a new book using the form. Try doing so from the last page of books. 
-  #       Your new book should show up immediately after you submit it at the end of the page. 
+  #       Your new book should show up immediately after you submit it at the end of the page.
+  @app.route('/books', methods=['POST'])
+  def create_book():
+    body = request.get_json()
+
+    new_title = body.get('title', None)
+    new_author = body.get('author', None)
+    new_rating = body.get('rating', None)
+
+    try:
+      book = Book(title=new_title, author=new_author, rating=new_rating)
+      book.insert()
+
+      selection = Book.query.order_by(Book.id).all()
+      current_books = paginate_books(request, selection)
+
+      return jsonify({
+        'success': True,
+        'created': book.id,
+        'books': current_books,
+        'total_books': len(Book.query.all())
+      })
+
+    except:
+      abort(422)
+
+
   
   return app
 
