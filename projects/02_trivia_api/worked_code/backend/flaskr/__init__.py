c04c1aca89d09d0365521626cf194cce1e698a30
André Quites Ordovás Santos <andrequites@gmail.com>
2021-10-25 15:43:13 -0300

    added * to CORS origins

--- projects/02_trivia_api/worked_code/backend/flaskr/__init__.py
+++ projects/02_trivia_api/worked_code/backend/flaskr/__init__.py
@@ -45,7 +45,8 @@ def create_app(test_config=None):
   '''
   # CORS Headers 
   @app.after_request
-  def after_request(response):    
+  def after_request(response):   
+    response.headers.add('Access-Control-Allow-Origin', '*') 
     response.headers.add('Access-Control-Allow-Headers', 'Content-Type,Authorization')
     response.headers.add('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
     return response
